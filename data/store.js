d275d6bd7bd738c9a9d8bc6b3e7d1bae6f43b131
Alex Benz <alex@alex-benz.de>
2019-08-12 14:07:04 +0200

    added basic store structure

---
+++ data/store.js
@@ -0,0 +1,16 @@
+const store = {
+  // contains the settings for the application
+  settings: {},
+  // constains the posts for the application
+  posts: {
+    total: 10,
+    current: 5,
+    count: 5,
+    data: [] // vielleicht kein array ?
+  },
+  //
+  tags: {
+    data: [] // vielleicht kein array ?
+  }
+  //
+};
